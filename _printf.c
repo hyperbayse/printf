111e9cc20343e7a989a10b27f0756ed675a8de6c
hyperbayse <hyperbayse@gmail.com>
2024-08-14 13:09:11 +0100

    update

--- _printf.c
+++ _printf.c
@@ -2,36 +2,36 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
-
+/**
+ * _printf - prints to the standard output
+ * @format: select which format to be used
+ * Return: number of bytes printed
+ */
 int _printf(const char *format, ...)
 {
 	int x, i, j;
 	char c;
-	char *s;
-	char *output;
+	char *s, *output;
 	va_list arg_p;
 
 	i = x = j = 0;
 
 	output = malloc(10 * sizeof(char));
-	if (output == NULL)
+	if (!(output))
 		return (1);
-
 	va_start(arg_p, format);
 	while (format[x] != '\0')
 	{
-		/* checks for format specifier */
 		if (format[x] == '%')
 		{
 			x++;
 			switch (format[x])
 			{
-			case 'c' :
+			case 'c':
 				c = va_arg(arg_p, int);
 				output[i++] = c;
 				break;
-			case 's' :
+			case 's':
 				s = va_arg(arg_p, char *);
 				while (s[j] != '\0')
 					output[i++] = s[j++];
@@ -41,17 +41,11 @@ int _printf(const char *format, ...)
 			}
 			j = 0;
 		}
-		/* copies information from format to output */
 		else
-		{
-			output[i] = format[x];
-			i++;
-		}
+			output[i++] = format[x];
 		x++;
 	}
 	va_end(arg_p);
-
-	/* print the stored strings to the standard output */
 	return (write(1, output, i));
 	free(output);
 }
